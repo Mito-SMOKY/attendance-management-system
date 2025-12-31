7267f18fdfda96f980492b7769bb67fc5db5933b
Maple428sugar <yuji.t18@icloud.com>
2025-12-31 12:49:10 +0900

    feat(admin):studentList Completed

--- attendancemanagementsystem/src/main/java/com/example/attendancemanagementsystem/common/repository/StudentRepository.java
+++ attendancemanagementsystem/src/main/java/com/example/attendancemanagementsystem/common/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.example.attendancemanagementsystem.common.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import com.example.attendancemanagementsystem.common.entity.StudentEntity;
 import com.example.attendancemanagementsystem.common.entity.UsersEntity;
 
 @Repository
-public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
+public interface StudentRepository extends JpaRepository<StudentEntity, Integer>, JpaSpecificationExecutor<StudentEntity> {
 
     // UsersEntity をもとに StudentEntity を検索するメソッド
     Optional<StudentEntity> findByUsers(UsersEntity users);
