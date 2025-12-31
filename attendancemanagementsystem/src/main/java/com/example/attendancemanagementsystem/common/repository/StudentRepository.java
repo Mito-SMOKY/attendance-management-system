d28043a44c9bacf3d94ba22f9f61716bb29c54bc
miyu-ai <miyu.20041014@icloud.com>
2025-12-31 21:02:44 +0900

    /fix:studentrepositoryにimport文の追加

--- attendancemanagementsystem/src/main/java/com/example/attendancemanagementsystem/common/repository/StudentRepository.java
+++ attendancemanagementsystem/src/main/java/com/example/attendancemanagementsystem/common/repository/StudentRepository.java
@@ -2,7 +2,7 @@ package com.example.attendancemanagementsystem.common.repository;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
