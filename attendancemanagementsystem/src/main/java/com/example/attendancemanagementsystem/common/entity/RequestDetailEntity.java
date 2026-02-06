31b29db7e7d111ac5e82303401755970a2717de7
Harachamu <kiyoaoto1@gmail.com>
2026-02-06 14:45:25 +0900

    feat(DB):これで華金に入れます

--- attendancemanagementsystem/src/main/java/com/example/attendancemanagementsystem/common/entity/RequestDetailEntity.java
+++ attendancemanagementsystem/src/main/java/com/example/attendancemanagementsystem/common/entity/RequestDetailEntity.java
@@ -12,7 +12,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "RequestDetail")
+@Table(name = "requestdetail")
 public class RequestDetailEntity {
 
     @Id
